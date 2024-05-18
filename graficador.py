41d6e6e1cd56db75c44fc73b051bbfd3902de133
fernang0 <fer.henriquez248@gmail.com>
2024-05-17 23:38:48 -0400

    graficador, grafica la frecuencia de palabras clave

---
+++ graficador.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#variables
+#ponle las palabras claves que quieras y te dara el grafico
+palabra_clave = ["conserje", "sale-entregado", "devuelto", "aparece-entregado", "no-entregado", "no-fue-entregado"];
+frecuencia = np.zeros(len(palabra_clave))
+links = open("datos.txt", "r");
+#lo chido
+for num in range(1, 1783, 1):
+    datos_linea = links.readline(num);
+    for id, palabra in enumerate(palabra_clave):
+        if palabra in datos_linea:
+            frecuencia[id] +=1;
+#se crea el grafico con los datos
+plt.figure(figsize=(10, 5))
+plt.barh(palabra_clave, frecuencia, color='salmon')
+plt.xlabel("Frecuencia")
+plt.ylabel("Palabras clave")
+plt.show()
